e1e852a8ce9583270f1144c6b78553b1529df885
Taeshawn Grogan <tae.grogan@drake.edu>
2025-04-22 22:44:02 -0500

    Removed sensitive and cache files from tracking

--- creds.py
+++
@@ -1,7 +0,0 @@
-rds_host = "project-one.cn9nbw41gsla.us-east-1.rds.amazonaws.com"
-db_user = 'admin'
-db_password = "I<3CS178!"
-db_name = 'movies'
-
-aws_access_key_id = 'AKIAT4GVRRPDBIXAYMXB'
-aws_secret_access_key = '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'
