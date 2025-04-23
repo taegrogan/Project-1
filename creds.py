d13f1f12bb3cc53449eb15ab2f04100b05249261
Taeshawn Grogan <tae.grogan@drake.edu>
2025-04-22 20:56:04 -0500

    Remove sensitive files from repo

--- creds.py
+++
@@ -1,8 +0,0 @@
-rds_host = 'movies.chgqog04mx8v.us-east-2.rds.amazonaws.com'
-db_user = 'admin'
-db_password = 'TaeGrogan0204$'
-db_name = 'movies'
-
-aws_access_key_id = 'AKIAT4GVRRPDDI'
-aws_secret_access_key = 'qVnXJm62BMXin5M8x5vttBBa2c1NrJL6qhlGeVqV'
-region_name = 'us-east-1'
